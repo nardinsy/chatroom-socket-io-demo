e128b4a70823d71e832bec9227ba633eafc50390
nardinsy <nasaya174@gmail.com>
2024-09-11 13:14:50 +0330

    Final commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,38 @@
 import express from "express";
-import { json } from "body-parser";
-import { configureCORS } from "./cors";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 
-const app = express();
-app.use(configureCORS);
+interface ServerToClientEvents {
+  "new-message": (message: string, username: string) => void;
+}
 
-app.use(json());
+interface ClientToServerEvents {
+  "hello-server": (usernameInput: string) => void;
+  "send-message": (message: string, username: string) => void;
+}
+
+type WSServer = Server<ClientToServerEvents, ServerToClientEvents>;
+
+type WSSocket = Socket<ClientToServerEvents, ServerToClientEvents>;
+
+const app = express();
 
 const server = createServer(app);
-const ws = new Server(server, {
+const ws: WSServer = new Server(server, {
   cors: {
     origin: ["http://localhost:3000", "https://admin.socket.io"],
   },
 });
 
-ws.on("connection", (socket) => {
+ws.on("connection", (socket: WSSocket) => {
   console.log("Connection established");
 
-  socket.on("Hello-Server", (username: string) => {
+  socket.on("hello-server", (username: string) => {
     console.log(`${username} say hello`);
   });
 
   socket.on("send-message", (message: string, username: string) => {
+    console.log(message);
     ws.emit("new-message", message, username);
   });
 });
